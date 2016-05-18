31d7f8d50f93d3e63181d056815cc4934ed61284
cpyou <745690939@qq.com>
2016-05-18 17:57:28 +0800

    tornado learn

---
+++ tornado_learn/hello.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# http://demo.pythoner.com/itt2zh/ch1.html#ch1-1-1
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    # handlers 声明Tornado应该用哪个类来响应请求
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
